0d90edee10d8ce58d5484311d31c32453b66d0ca
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-02-17 11:45:53 +0100

    feat: change hello world

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { sleep } from "./sleep";
 
 export const main = async () => {
   core.info("Running github-action-nodejs-template");
-  core.info("Hello world");
+  core.info("Hello world 2");
   await sleep(3000);
   core.info("Finished github-action-nodejs-template");
 };
