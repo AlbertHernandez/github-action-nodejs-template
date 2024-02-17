c1e0c322e5e06931eaa7d0f7cd124ca3de0f0542
Albert <alberthernandezdev@gmail.com>
2024-02-17 17:11:10 +0100

    fix: write log change

--- dist/index.js
+++ dist/index.js
@@ -24722,6 +24722,7 @@ class Action {
         const name = inputs.name || "World";
         this.logger.info(`Hello ${name}`);
         await this.sleep(3000);
+        this.logger.info("Change: 1");
         this.logger.info("Finished github-action-nodejs-template");
     }
     sleep(milliseconds) {
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ const config = {
   "**/*.ts?(x)": () => "tsc -p tsconfig.prod.json --noEmit",
   "*.{js,jsx,ts,tsx}": [
     "npm run lint",
-    "jest --findRelatedTests --passWithNoTests",
+    "jest --coverage=false --findRelatedTests --passWithNoTests",
   ],
   "*.{md,json}": "prettier --write",
 };
