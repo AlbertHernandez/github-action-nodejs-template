d4e8bc7ae3d1df9592ecfafac6de5e9ad01288f6
Albert <alberthernandezdev@gmail.com>
2024-02-17 15:21:37 +0100

    fix: change hello world

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { sleep } from "./sleep";
 
 export const main = async () => {
   core.info("Running github-action-nodejs-template");
-  core.info("Hello world 18");
+  core.info("Hello world 19");
   await sleep(3000);
   core.info("Finished github-action-nodejs-template");
 };
