9cba26c22bd0d5d6d7665309aba564df29f3cc1c
Albert <alberthernandezdev@gmail.com>
2024-02-18 15:40:11 +0100

    chore: change number

--- src/action.ts
+++ src/action.ts
@@ -13,7 +13,7 @@ export class Action {
     const name = inputs.name || "World";
     this.logger.info(`Hello ${name}`);
     await this.sleep(3000);
-    this.logger.info("Change: 8");
+    this.logger.info("Change: 9");
     this.logger.info("Finished github-action-nodejs-template");
   }
 
